2885052424a2395d89906ce6f0db20ee497b109c
rickingyn <rickingyn@gmail.com>
2020-04-24 14:24:06 -0400

    add navlinks and search form, installed axios and fetch api
    Added navlinks and search form the Nav component. Installed axios to
    fetch API using apiKey

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 import Router from './components/Route';
 import Nav from './components/Nav';
+import apiKey from './config';
 
-const App = () => {
-  return(
-      <BrowserRouter>
-        <Nav />
-        <Router />
-      </BrowserRouter>
-  );
+class App extends Component {
+  state = {
+    api: []
+  };
+
+  componentDidMount() {
+    const URL = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=sunsets.&per_page=&format=json&nojsoncallback=1`;
+
+    axios.get(URL)
+      .then(response => 
+        console.log(response.data))
+      .catch(error => console.log(error));
+  }
+
+  render() {
+      return( 
+          <BrowserRouter>
+            <Nav />
+            <Router />
+          </BrowserRouter>
+      );
+  }
 }
 
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
+import SearchForm from './SearchForm';
+import '../css/index.css';
 
 const Nav = () => {
     return(
         <div>
+            <SearchForm />
 
+            {/* Nav Links */}
+            <nav className='main-nav'>
+                <ul>
+                    <li><NavLink exact to='/'>Cats</NavLink></li>
+                    <li><NavLink exact to='/'>Dogs</NavLink></li>
+                    <li><NavLink exact to='/'>Computers</NavLink></li>
+                </ul>
+
+            </nav>
         </div>
     );
 };
